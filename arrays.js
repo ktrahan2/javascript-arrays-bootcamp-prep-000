3926e21ef8bd80ab9080d3fd499b447c213352f4
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-10 20:48:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,1 @@
 var chocolateBars = ["snickers", "hundredgrand", "kitkat", "skittles"]
-
-function addElementToBeginningOfArray(array, "element") {
-  ["element",...array]
-  return addElementToBeginningOfArray
-}
