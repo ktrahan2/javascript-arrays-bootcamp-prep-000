e9cc9d1cc9bdb1d4b35ae6858fe51312c1421cca
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-10 20:52:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
+function addElementToBeginningOfArray(array, element) {
+  [element,...array]
+  var newadd = [element,...array]
+  return newadd
+}
