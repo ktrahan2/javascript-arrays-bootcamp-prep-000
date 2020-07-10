da8a6621e420a081e6a2f7931c325ff7e2aabd9c
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-10 20:50:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,5 @@ var chocolateBars = ["snickers", "hundredgrand", "kitkat", "skittles"]
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
+  return array
 }
